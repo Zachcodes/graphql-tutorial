e6a29faff09da401b14932fd6894e95283aff62c
zach <zaspringer@gmail.com>
2019-01-20 13:27:56 -0700

    Added all of the functionality to the prisma database instance. Definitely a much more solid understanding of how this works

--- src/index.js
+++ src/index.js
@@ -1,47 +1,28 @@
 const { GraphQLServer} = require('graphql-yoga')
 const { prisma } = require('./generated/prisma-client')
 
+//resolvers 
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+
 const resolvers = {
-    Query: {
-        info: () => 'Thwis is the api of a hackernews clone',
-        feed: (root, args, context, info) => {
-            return context.prisma.links()
-        },
-        link: (parent, args) => {
-            return links.find( link => link.id === args.id)
-        }
-    },
-    Mutation: {
-        post: (root, args, context) => {
-            return context.prisma.createLink({
-                url: args.url,
-                description: args.description
-            })
-        },
-        updateLink: (parent, args) => {
-            let linkToUpdate = links.find( link => link.id === args.id)
-            if(linkToUpdate) {
-                linkToUpdate = {
-                    id: linkToUpdate.id,
-                    url: args.url ? args.url : linkToUpdate.url,
-                    description: args.description ? args.description : linkToUpdate.description
-                }
-                return linkToUpdate
-            }
-            else return null;
-        },
-        deleteLink: (parent, args) => {
-            let index = links.findIndex( link => link.id === args.id)
-            if(index !== -1) links.splice(index, 1)
-            return links;
-        }
-    }
+    Query,
+    Mutation,
+    User,
+    Link
 }
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
-    context: {prisma}
+    context: request => {
+        return {
+            ...request,
+            prisma
+        }
+    } 
 })
 
 server.start(() => console.log('Server is up and running on localhost 4000'))
